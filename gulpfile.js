40f64ea31f0a2f9ad639de3664b9650767e6c3eb
Rachel Keslensky <rach@lid.cc>
2016-01-26 12:19:38 -0800

    chore: set up gulp, sass, and browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('hello', function() {
+  console.log('Hello Zell');
+});
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  // Other watchers
+});
